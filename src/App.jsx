9fa98a18175c2fe02d8722f7c9b1a97552d60047
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-11 13:59:49 +0300

    feat: write FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-function App() {
+import { useState } from 'react'
+import { TypeAnimation } from 'react-type-animation'
+
+export default function App() {
+
     return (
         <div>
             <video autoPlay muted playsInline loop>
                 <source src="dog.mp4" />
             </video>
-            <div className="absolute z-10 ml-[5%] mt-[13%] font-windows-terminal text-5xl text-white">
-                <ul id="social-media" className="mb-[8%] flex flex-col gap-2">
-                    <li>C:\&gt;doomer_dog</li>
-                    <li>&gt;twitter</li>
-                    <li>&gt;telegram</li>
-                    <li>&gt;pumpfun</li>
-                </ul>
-                <ul id="faq">
-                    <li>C:\&gt;FAQ</li>
-                    <li>&gt;he has no friend</li>
-                    <li>&gt;he has no personal life</li>
-                    <li>&gt;he gambles on pumpfun 24/7</li>
-                    <li>&gt;he is just like you, anon</li>
-                </ul>
+            <div className="absolute z-10 ml-[1%] mt-[9.5%] font-windows-terminal text-7xl text-white">
+                <SocialMedia />
+                <Faq />
             </div>
             <video autoPlay muted playsInline loop>
                 <source src="terminal.mov" />
@@ -29,4 +22,75 @@ function App() {
     )
 }
 
-export default App
+
+const typingSpeed = 30
+
+function Faq() {
+    return (
+
+        <TypeAnimation
+            sequence={[
+                250,
+                "C:\>FAQ\n", 700,
+                "C:\>FAQ\n" + ">he has no friends\n", 700,
+                "C:\>FAQ\n" + ">he has no friends\n" + ">he has no personal life\n", 700,
+                "C:\>FAQ\n" + ">he has no friends\n" + ">he has no personal life\n" + ">he gambles on pumpfun 24/7\n", 800,
+                "C:\>FAQ\n" + ">he has no friends\n" + ">he has no personal life\n" + ">he gambles on pumpfun 24/7\n" + ">he is just like you, anon",
+
+            ]}
+            wrapper="span"
+            speed={typingSpeed}
+            style={{ whiteSpace: 'pre-line', }}
+            repeat={0}
+        />
+    )
+}
+
+function SocialMedia() {
+
+    // let [text, setText] = useState('C:>doomer_dog\n')
+
+    return (
+        <>
+            <TypeAnimation
+                sequence={[
+                    "C:>doomer_dog", (thiz) => showCursorAnimation(thiz, false)
+                    //        (()=> {text += '>telegram\n'}),
+                    // text , 500,
+                    // 'C:>doomer_dog\n' + '>pumpfun\n' + '>telegram\n' + '>twitter', 1500, 
+                    // 'C:>doomer_dog\n' + '>pumpfun\n' + '>telegram\n' + '>X', 
+                    // 'C:>doomer_dog\n' + '>pumpfun\n' + '>telegram\n' + '>X\n\n',
+                    // 'C:>doomer_dog\n' + '>pumpfun\n' + '>telegram\n' + '>X\n\n',
+                    // C:\>FAQ
+                    // >he has no friend
+                    // >he has no personal life
+                    // >he gambles on pumpfun 24/7
+                    // >he is just like you, anon
+                    //
+                    // whiteSpace: 'pre-line',
+                ]}
+                wrapper="span"
+                speed={typingSpeed}
+                style={{ display: 'flex', flexDirection: "column" }}
+                repeat={0}
+            />
+        </>
+    )
+}
+
+
+const CURSOR_CLASS_NAME = 'custom-type-animation-cursor';
+
+function showCursorAnimation(ref, isShown) {
+    if (!ref.current) {
+        return;
+    }
+
+    console.log("hell")
+    const el = ref.current;
+    if (isShown) {
+        el.classList.add(CURSOR_CLASS_NAME);
+    } else {
+        el.classList.remove(CURSOR_CLASS_NAME);
+    }
+};
