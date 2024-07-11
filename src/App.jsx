ec7374c4b2a4ec2e5ba318fe5936d250dd451fbc
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-11 09:43:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <video autoPlay muted playsInline loop>
+        <source src="dog.mp4" />
+      </video>
+      <video autoPlay muted playsInline loop>
+        <source src="terminal.mov" />
+      </video>
+      <video autoPlay muted playsInline loop>
+        <source src="dog-on-pc.mov" />
+      </video>
+    </div>
+  );
+}
+
+export default App;
