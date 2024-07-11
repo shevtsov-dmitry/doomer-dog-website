ad4dfc6ec51eaa7239ea05b38898b1da962c3cab
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-11 12:36:28 +0300

    fix: make font work

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ function App() {
             <video autoPlay muted playsInline loop>
                 <source src="dog.mp4" />
             </video>
-            <div className="absolute ml-12 mt-48 font-windows-terminal text-2xl font-bold text-white z-10">
-                <ul id="social-media">
-                    <li>C:\&#10095;doomer_dog</li>
-                    <li>&#10095;twitter</li>
-                    <li>&#10095;telegram</li>
-                    <li>&#10095;pumpfun</li>
+            <div className="absolute z-10 ml-[5%] mt-[13%] font-windows-terminal text-5xl text-white">
+                <ul id="social-media" className="mb-[8%] flex flex-col gap-2">
+                    <li>C:\&gt;doomer_dog</li>
+                    <li>&gt;twitter</li>
+                    <li>&gt;telegram</li>
+                    <li>&gt;pumpfun</li>
                 </ul>
                 <ul id="faq">
-                    <li>C:\&#10095;FAQ</li>
-                    <li>&#10095;twitter</li>
-                    <li>&#10095;telegram</li>
-                    <li>&#10095;pumpfun</li>
+                    <li>C:\&gt;FAQ</li>
+                    <li>&gt;he has no friend</li>
+                    <li>&gt;he has no personal life</li>
+                    <li>&gt;he gambles on pumpfun 24/7</li>
+                    <li>&gt;he is just like you, anon</li>
                 </ul>
             </div>
             <video autoPlay muted playsInline loop>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        "windows-terminal": ["windows_command_prompt", "monospace"],
-      },
-      screens: {
-        laptop: "1700px",
-        mobile: "1133px",
-      },
-      colors: {},
+    content: ['./src/**/*.{js,jsx,ts,tsx,css}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'windows-terminal': ['Windows Command Prompt', 'Serif'],
+            },
+            screens: {
+                laptop: '1700px',
+                mobile: '1133px',
+            },
+            colors: {},
+        },
     },
-  },
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+    plugins: ['prettier-plugin-tailwindcss'],
+}
