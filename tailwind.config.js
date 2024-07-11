20a3f87ebc4be3fcfde00870a7f8a051ea65e11e
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-11 10:17:50 +0300

    add new font support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "windows-terminal": ["windows_command_prompt", "monospace"],
+      },
+      screens: {
+        laptop: "1700px",
+        mobile: "1133px",
+      },
+      colors: {},
     },
-    plugins: ['prettier-plugin-tailwindcss'],
-}
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+};
